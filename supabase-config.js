859d5cf8ca7b9a204d2b98289398886063df7d1f
codounette1986 <codou.ndiaye@gmail.com>
2026-04-16 14:39:57 +0000

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.IAJUICE_SUPABASE = {
+  url: "https://whanmnlirtolyucbpwiq.supabase.co",
+  anonKey: "sb_publishable_lKZJZnjWzS6hwj_MjzsZOg_8EhgiovQ",
+};
--- sw.js
+++ sw.js
@@ -1,12 +1,16 @@
-const CACHE_NAME = "iajuice-pwa-v2";
+const CACHE_NAME = "iajuice-pwa-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/logo-iajuice.jpeg"];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
 });
 
 self.addEventListener("fetch", (event) => {
